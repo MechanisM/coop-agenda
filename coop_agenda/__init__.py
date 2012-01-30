9895e6ec90e31a6e2b1b0d30f07615cfef5fc766
Dominique Guardiola <contact@quinode.fr>
2012-01-30 19:14:08 +0100

    setup

--- coop_agenda/__init__.py
+++ coop_agenda/__init__.py
@@ -1,16 +1,7 @@
-VERSION = (0, 2, 0)
-
+VERSION = (0, 1, 0)
 
 def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
-    else:
-        version = '%s %s' % (version, VERSION[3])
-        if VERSION[3] != 'final':
-            version = '%s %s' % (version, VERSION[4])
+    version = '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2])
     return version
 
 __version__ = get_version()
